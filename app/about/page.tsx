13598bb36bcc0a1b2ab52de60336ebb9f9bfb192
Sebastian Moder <moder.sebastian@gmail.com>
2023-10-08 22:27:12 +0200

    Update About page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,10 @@ import Typography from "@mui/material/Typography";
 export default function About() {
   return (
     <div>
-      <Typography variant="h1">Main</Typography>
+      <Typography variant="h1">About</Typography>
+      <Typography variant="body1">
+        Pathfinder is a website for an Erasmus+ project.
+      </Typography>
     </div>
   );
 }
