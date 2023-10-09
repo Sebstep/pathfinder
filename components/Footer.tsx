1e7f7a6bc64069f927031fe948bc3d7823d1feb2
Sebastian Moder <moder.sebastian@gmail.com>
2023-10-09 15:35:50 +0200

    Add Footer component for website

---
+++ components/Footer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 w-full bg-gray-800 py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-between items-center">
+          <div className="w-full md:w-auto text-center md:text-left mb-4 md:mb-0">
+            <p className="text-white">&copy; 2021 My Website</p>
+          </div>
+          <div className="w-full md:w-auto text-center md:text-right">
+            <Link href="#" className="text-white hover:text-gray-400 mx-2">
+              Privacy Policy
+            </Link>
+            <Link href="#" className="text-white hover:text-gray-400 mx-2">
+              Terms of Service
+            </Link>
+            <Link href="#" className="text-white hover:text-gray-400 mx-2">
+              Contact Us
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
