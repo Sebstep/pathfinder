bd145a25c94f998a0c2193d8fed21d53fbf6d016
Sebastian Moder <moder.sebastian@gmail.com>
2023-10-09 15:36:14 +0200

    Add Footer component for website

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,16 @@ import * as React from "react";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "GPT-3 Playground",
-  description: "A playground for GPT-3",
+  title: "Pathfinder",
+  description: "An Erasmus+ project for higher education",
 };
+
+// TODO: The content currently scrolls behind the footer.
 
 export default function RootLayout({
   children,
@@ -18,9 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head></head>
       <body className={inter.className}>
         <Navigation />
         <main className="container mx-auto">{children}</main>
+        <Footer />
       </body>
     </html>
   );
