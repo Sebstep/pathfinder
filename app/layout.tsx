db71efcd6d5d188a153cce04652268938b636454
Sebastian Moder <moder.sebastian@gmail.com>
2023-10-10 15:11:02 +0200

    Remove unnecessary import and update main className

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import * as React from "react";
-import Head from "next/head";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import Navigation from "@/components/Navigation";
@@ -24,10 +23,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/icon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>
         <Navigation />
-        <main className="container mx-auto">{children}</main>
+        <main className="mx-auto">{children}</main>
         <Footer />
       </body>
     </html>
