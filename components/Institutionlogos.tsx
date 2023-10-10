b2aca137cce34cc8339986e71cb0ac38b7fba069
Sebastian Moder <moder.sebastian@gmail.com>
2023-10-10 20:34:07 +0200

    institution logos

--- components/Institutionlogos.tsx
+++ components/Institutionlogos.tsx
@@ -7,17 +7,17 @@ export default function InstitutionLogos() {
     <div className="mx-auto py-8">
       <div className="flex flex-wrap justify-center items-center">
         {/* Replace the paths with the actual paths to your institution logos */}
-        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 p-4 bg-yellow-400">
-          {/* <Image src="/institution1.png" alt="Institution 1" className="mx-auto" /> */}
+        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 p-4 mx-4">
+          <Image src="/images/logoipsum-235.svg" alt="Institution 1" width={300} height={200} className="mx-auto" />
         </div>
-        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 p-4 bg-red-400">
-          {/* <Image src="/institution2.png" alt="Institution 2" className="mx-auto" /> */}
+        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 p-4 mx-4">
+          <Image src="/images/logoipsum-243.svg" alt="Institution 2" width={300} height={200} className="mx-auto" />
         </div>
-        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 p-4 bg-blue-400">
-          {/* <Image src="/institution3.png" alt="Institution 3" className="mx-auto" /> */}
+        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 p-4 mx-4">
+          <Image src="/images/logoipsum-251.svg" alt="Institution 3" width={300} height={200} className="mx-auto" />
         </div>
-        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 p-4 bg-lime-400">
-          {/* <Image src="/institution4.png" alt="Institution 4" className="mx-auto" /> */}
+        <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 p-4 mx-4">
+          <Image src="/images/logoipsum-297.svg" alt="Institution 4" width={300} height={200} className="mx-auto" />
         </div>
       </div>
     </div>
