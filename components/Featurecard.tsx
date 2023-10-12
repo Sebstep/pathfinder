5cc0b7cbbe9bea185644b93de588ae104b16d37a
Sebastian Moder <moder.sebastian@gmail.com>
2023-10-12 10:26:13 +0200

    hover effect on featurecards

--- components/Featurecard.tsx
+++ components/Featurecard.tsx
@@ -10,23 +10,21 @@ type FeaturecardProps = {
 
 export default function Featurecard(props: FeaturecardProps) {
   return (
-    <div className="p-4 md:w-1/3">
-      <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-        <div className="flex items-center mb-3">
-          <div className="w-12 h-12 mr-4 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+    <Link href={props.link} className="p-4 md:w-1/3 ">
+      <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col group border border-transparent px-5 py-4 transition-colors hover:border-gray-500 hover:bg-gray-300">
+        <div className="flex items-center mb-3 p-2">
+          <div className="w-12 h-12 mr-4 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0 transition-transform group-hover:-translate-y-1 motion-reduce:transform-none">
             {props.icon}
           </div>
           <h3>{props.title}</h3>
         </div>
         <div className="flex-grow">
-          <p className="leading-relaxed text-base">
-            {props.description}
-          </p>
-          <Link href={props.link} className="mt-3 text-indigo-500 inline-flex items-center">
+          <p className="leading-relaxed text-base">{props.description}</p>
+          <p className="mt-3 text-indigo-500 inline-flex items-center">
             Learn More -&gt;
-          </Link>
+          </p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
