ba2652ae6763cab87a14898fdc3e42fd835386df
Sebastian Moder <moder.sebastian@gmail.com>
2023-10-12 11:24:21 +0200

    refactored headings

--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,9 @@ import { GiWizardStaff, GiBorderedShield, GiBroadsword } from "react-icons/gi";
 import InstitutionLogos from "@/components/Institutionlogos";
 import Box from "@mui/material/Box";
 import Featurecard from "@/components/Featurecard";
+import Headings from "@/components/Doubleheadings";
 
 export default function Home() {
-  function Headings(props: { top: string; bottom: string }) {
-    return (
-      <div className="flex flex-col text-center w-full mb-8">
-        <h3 className="text-xs text-blue-500 tracking-widest font-medium text-center mb-1">
-          {props.top}
-        </h3>
-        <h2 className="sm:text-3xl text-2xl font-medium title-font">
-          {props.bottom}
-        </h2>
-      </div>
-    );
-  }
-
   return (
     <div className="">
       {/* Hero Section */}
