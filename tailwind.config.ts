eb76fed4b25252c0105a5f3e2718251b35012f1c
Sebastian Moder <moder.sebastian@gmail.com>
2024-03-07 00:49:51 +0100

    Add new EU flag colors to tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "eu-blue": "#004494",
+        "eu-yellow": "#ffd617",
+        "eu-grey": "#404040",
+      }
     },
   },
 }
