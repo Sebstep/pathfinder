1408ef77fbade7e6b7fb25b61dd50fae1f3cb765
Sebastian Moder <moder.sebastian@gmail.com>
2024-03-07 01:40:13 +0100

    added demo content

--- app/sage/page.tsx
+++ app/sage/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-import Rhf from "@/components/forms/Rhf";
-import Typography from "@mui/material/Typography";
+import Headings from "@/components/Doubleheadings";
 
 export default function Home() {
-  return <h1>SAGE</h1>;
+  return (
+    <section className="py-16">
+      <div className="w-1/2 mx-auto">
+        <Headings
+          top="SAGE"
+          bottom="Strategies for AI-Guided Education"
+        />
+        <p className="mb-8">
+          The SAGE framework offers educators a detailed roadmap to navigate AI-guided
+          education. It provides a step-by-step guide that supports AI technology
+          integration and promotes a deep understanding of its impact on student learning.
+        </p>
+        <p className="mb-8">
+          The SAGE framework encourages educators to embrace innovation by exploring new
+          teaching methodologies and tools that capitalise on AI advancements. This
+          approach enhances the learning experience and prepares students for the
+          challenges and opportunities of the future by equipping them with essential
+          skills such as problem-solving and critical thinking.
+        </p>
+      </div>
+    </section>
+  );
 }
