143aaf7010d1e5e96de7ab78a05de3ccfcd95146
Sebastian Moder <moder.sebastian@gmail.com>
2024-03-07 01:39:59 +0100

    Update project page content and add objectives section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Typography } from "@mui/material";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import { GiWizardStaff, GiBorderedShield, GiBroadsword } from "react-icons/gi";
 import InstitutionLogos from "@/components/Institutionlogos";
-import Box from "@mui/material/Box";
 import Featurecard from "@/components/Featurecard";
 import Headings from "@/components/Doubleheadings";
 
@@ -19,38 +12,58 @@ export default function Home() {
 
       <section className="py-16">
         <div className="container mx-auto px-5 flex flex-col items-center md:flex-row">
-
           {/* Left Side */}
           <div className="text-center flex flex-col items-center md:pr-16 md:items-start md:text-left lg:flex-grow md:w-1/2 lg:pr-24">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
-              Welcome to the<br />Erasmus+ Pathfinder Project
+              Welcome to the
+              <br />
+              Erasmus+ Pathfinder Project
             </h1>
             <p className="mb-8 leading-relaxed">
-              Pioneering AI Technology in Higher Education to Facilitate
-              Innovation and Nurture the Development of Entrepreneurial
-              Resources
+              Pioneering AI Technology in Higher Education to Facilitate Innovation and
+              Nurture the Development of Entrepreneurial Resources
             </p>
-            <p>Project Number: <a href="https://erasmus-plus.ec.europa.eu/projects/search/details/2023-2-LI01-KA220-HED-000178579">2023-2-LI01-KA220-HED-000178579</a></p>
           </div>
 
           {/* Right Side */}
           <div className="w-5/6 md:w-1/2 lg:max-w-lg lg:w-full">
             <Image
-              src="/images/AdobeStock_622648834.jpeg"
+              src="/images/hero-image.jpeg"
               alt="Hero Image"
               width={600}
               height={400}
               className="object-cover object-center rounded-2xl"
             />
           </div>
-
         </div>
       </section>
 
-      {/* Feature Section */}
+      {/* <!-- Objectives Section --> */}
+      <section className="py-16 bg-slate-100">
+        <div className="w-1/2 mx-auto">
+          <Headings
+            top="PROJECT OBJECTIVE"
+            bottom="Empowering Education Through AI"
+          />
+          <div className="text-center">
+            <p className="mb-8">
+              Our goal is to enhance AI understanding in higher education, equipping
+              educators and students with resources for its responsible use. We aim to
+              boost digital readiness, foster innovation, and improve employability
+              through innovative educational strategies and entrepreneurial skill
+              development.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/* Deliverables Section */}
       <section className="py-16">
         <div className="container px-5 mx-auto">
-          <Headings top="PROJECT DELIVERABLES" bottom="Meet the Pathfinders" />
+          <Headings
+            top="PROJECT DELIVERABLES"
+            bottom="Meet the Pathfinders"
+          />
           <div className="flex flex-wrap -m-4">
             <Featurecard
               title="SAGE"
@@ -75,21 +88,18 @@ export default function Home() {
         </div>
       </section>
 
-      {/* <!-- About Section --> */}
-      <section className="mx-auto py-16">
+      {/* <!-- Contact Section --> */}
+      <section className="py-16 mx-auto">
         <Headings
           top="PROJECT CONSORTIUM"
           bottom="Participating Organizations"
         />
         <div className="text-center">
           <InstitutionLogos />
-          <p className="mb-8">
-            Feel free to get in touch with us for more information.
-          </p>
+          <p className="mb-8">Feel free to get in touch with us for more information.</p>
           <Link
             href="/about"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          >
+            className="bg-eu-blue hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full">
             Contact Us
           </Link>
         </div>
