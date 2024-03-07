3a33fdffd82f14f09ac2d7c44b2f80850e7bf77d
Sebastian Moder <moder.sebastian@gmail.com>
2024-03-07 01:38:27 +0100

    added demo content

--- app/artisan/page.tsx
+++ app/artisan/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
-import Rhf from "@/components/forms/Rhf";
-import Typography from "@mui/material/Typography";
+import Headings from "@/components/Doubleheadings";
 
 export default function Home() {
-  return <h1>ARTISAN</h1>;
+  return (
+    <section className="py-16">
+      <div className="w-1/2 mx-auto">
+        <Headings
+          top="ARTISAN"
+          bottom="AI Readiness Training for Insightful Systematic Adaptation and
+          Navigation"
+        />
+        <p className="mb-8">
+          ARTISAN aims to promote AI literacy, foster an entrepreneurial mindset, and
+          effectively empower students to integrate AI technology into their study
+          practices. Additionally, ARTISAN seeks to enhance students&apo; understanding of
+          the ethical implications of AI and encourage exploration of AI&apo;s potential.
+        </p>
+        <p className="mb-8">
+          This innovative toolkit will be based on the DigComp 2.2 framework and grounded
+          in the EU&apo;s 2022 AI ethics guidelines and offer students extensive training
+          resources and materials to facilitate comprehensive learning. Utilising the
+          ARTISAN learning journey will empower students to enhance their AI literacy and
+          equip them with the skills needed to effectively integrate AI technology into
+          their study practices, thus enhancing their overall educational experience.
+        </p>
+      </div>
+    </section>
+  );
 }
