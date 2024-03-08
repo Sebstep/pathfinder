47157d7d594755e36809c92980d1560084af32a6
Sebastian Moder <moder.sebastian@gmail.com>
2024-03-08 14:44:57 +0100

    refactored partners

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
+import { ReactNode } from "react";
 import Image from "next/image";
 import Headings from "@/components/Doubleheadings";
 import {
@@ -11,260 +11,204 @@ import {
   FaLink,
 } from "react-icons/fa";
 
+interface SocialLink {
+  name: string;
+  url: string;
+  icon: JSX.Element;
+}
+
+interface Partner {
+  name: string;
+  country: string;
+  description: string;
+  imgSrc: string;
+  socialLinks: SocialLink[];
+}
+
+const partners: Partner[] = [
+  // Partner 1
+  {
+    name: "University of Liechtenstein",
+    country: "Liechtenstein 🇱🇮",
+    description:
+      "The University of Liechtenstein specializes in architecture and business economics, offering an intimate academic environment with a strong focus on sustainability and innovation.",
+    imgSrc: "/images/partners/unili.png",
+    socialLinks: [
+      { name: "Website", url: "https://www.uni.li/en/", icon: <FaGlobe size={24} /> },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/school/universit-t-liechtenstein/",
+        icon: <FaLinkedin size={24} />,
+      },
+      {
+        name: "Facebook",
+        url: "https://www.facebook.com/uni.li",
+        icon: <FaFacebook size={24} />,
+      },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/uni_li",
+        icon: <FaInstagram size={24} />,
+      },
+    ],
+  },
+  // Partner 2
+  {
+    name: "LAB University of Applied Sciences",
+    country: "Finland 🇫🇮",
+    description:
+      "LAB University of Applied Sciences in Finland stands out for its applied research and education in health and wellbeing, circular economy, and design, preparing students for impactful careers with a global perspective.",
+    imgSrc: "/images/partners/lab.png",
+    socialLinks: [
+      { name: "Website", url: "https://lab.fi/en/", icon: <FaGlobe size={24} /> },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/school/labfinland/",
+        icon: <FaLinkedin size={24} />,
+      },
+      {
+        name: "Facebook",
+        url: "https://www.facebook.com/LABfinland",
+        icon: <FaFacebook size={24} />,
+      },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/labfinland/",
+        icon: <FaInstagram size={24} />,
+      },
+    ],
+  },
+  // Partner 3
+  {
+    name: "University of Dubrovnik",
+    country: "Croatia 🇭🇷",
+    description:
+      "The University of Dubrovnik, Croatia, renowned for its maritime studies, electrical engineering, and business courses, combines rich historical heritage with modern educational approaches.",
+    imgSrc: "/images/partners/unidu.png",
+    socialLinks: [
+      { name: "Website", url: "https://www.unidu.hr/eng/", icon: <FaGlobe size={24} /> },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/school/unidu-cro/",
+        icon: <FaLinkedin size={24} />,
+      },
+      {
+        name: "Facebook",
+        url: "https://www.facebook.com/UNIDUcro/",
+        icon: <FaFacebook size={24} />,
+      },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/sveucilisteudubrovniku/",
+        icon: <FaInstagram size={24} />,
+      },
+    ],
+  },
+  // Partner 4
+  {
+    name: "Formatio Private School",
+    country: "Liechtenstein 🇱🇮",
+    description:
+      "Formatio Private School was founded in 1995 and pioneers personalized education, fosters strengths, and cultivates digital proficiency, preparing students for the 21st-century landscape with excellence and innovation.",
+    imgSrc: "/images/partners/formatio.png",
+    socialLinks: [
+      {
+        name: "Website",
+        url: "https://www.formatio.li/en/",
+        icon: <FaGlobe size={24} />,
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/company/formatio-privatschule/",
+        icon: <FaLinkedin size={24} />,
+      },
+      {
+        name: "Facebook",
+        url: "https://www.facebook.com/formatioprivatschule",
+        icon: <FaFacebook size={24} />,
+      },
+      {
+        name: "Instagram",
+        url: "https://www.instagram.com/formatio_privatschule/",
+        icon: <FaInstagram size={24} />,
+      },
+    ],
+  },
+];
+
+function SocialIcons({ links }: { links: SocialLink[] }) {
+  return (
+    <span className="inline-flex">
+      {links.map((link, index) => (
+        <a
+          key={index}
+          className="text-gray-500 hover:text-eu-blue mr-4"
+          href={link.url}
+          rel="noopener noreferrer"
+          target="_blank">
+          {link.icon}
+        </a>
+      ))}
+    </span>
+  );
+}
+
+function PartnerWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="p-8 xl:w-1/2">
+      <div className="px-8 h-full flex flex-col sm:flex-row items-start justify-center text-left">
+        {children}
+      </div>
+    </div>
+  );
+}
+
 export default function About() {
   return (
-    <section className="">
-      <div className="container py-16 mx-auto">
-        {/* Top Part Wrapper */}
-        <div className="flex flex-col mb-16 w-5/6 md:w-1/2 lg:max-w-2xl lg:w-full mx-auto">
-          <Headings
-            top="PROJECT CONSORTIUM"
-            bottom="Participating Organizations"
-          />
-          <p className="mb-8 leading-relaxed">
-            Pathfinder is a collaborative Erasmus+ project that brings together leading
-            institutions in higher education to address the challenges and opportunities
-            presented by Artificial Intelligence (AI) in the educational landscape. Our
-            consortium comprises esteemed universities and institutions from across
-            Europe, each contributing their unique expertise and perspectives to this
-            transformative initiative.
-          </p>
-          <p className="mb-8 leading-relaxed">
-            Our approach is guided by the principles of collaboration, knowledge-sharing,
-            and responsible innovation. We strive to create a future where AI is embraced
-            as a powerful tool for enhancing educational experiences and preparing
-            learners for the digital era.
-          </p>
-        </div>
+    <section className="container py-16 mx-auto">
+      {/* Top Part Wrapper */}
+      <div className="flex flex-col mb-16 w-5/6 md:w-1/2 lg:max-w-2xl lg:w-full mx-auto">
+        <Headings
+          top="PROJECT CONSORTIUM"
+          bottom="Participating Organizations"
+        />
+        <p className="mb-8 leading-relaxed">
+          Pathfinder is a collaborative Erasmus+ project that brings together leading
+          institutions in higher education to address the challenges and opportunities
+          presented by Artificial Intelligence (AI) in the educational landscape. Our
+          consortium comprises esteemed universities and institutions from across Europe,
+          each contributing their unique expertise and perspectives to this transformative
+          initiative.
+        </p>
+        <p className="mb-8 leading-relaxed">
+          Our approach is guided by the principles of collaboration, knowledge-sharing,
+          and responsible innovation. We strive to create a future where AI is embraced as
+          a powerful tool for enhancing educational experiences and preparing learners for
+          the digital era.
+        </p>
+      </div>
 
-        {/* Partnership Wrapper */}
-        <div className="flex flex-wrap -m-4">
-          {/* Partner 1 */}
-          <div className="p-4 lg:w-1/2">
-            <div className="h-full flex sm:flex-row flex-col items-start sm:justify-start justify-center text-center sm:text-left">
-              <Image
-                alt="team"
-                className="flex-shrink-0 w-48 object-contain object-center sm:mb-0 mb-4"
-                width={200}
-                height={200}
-                src="/images/partners/unili.png"
-              />
-              <div className="flex-grow sm:pl-8">
-                <h2 className="title-font font-medium text-lg text-gray-900">
-                  University of Liechtenstein
-                </h2>
-                <h3 className="text-gray-500 mb-3">Liechtenstein 🇱🇮</h3>
-                <p className="mb-4">
-                  The University of Liechtenstein specializes in architecture and business
-                  economics, offering an intimate academic environment with a strong focus
-                  on sustainability and innovation.
-                </p>
-                <span className="inline-flex">
-                  <a
-                    className="text-gray-500 hover:text-eu-blue"
-                    href="mailto:sebastian.moder@uni.li"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaEnvelope size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.uni.li/en/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaGlobe size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.facebook.com/uni.li"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaFacebook size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.instagram.com/uni_li"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaInstagram size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.linkedin.com/school/universit-t-liechtenstein/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaLinkedin size={24} />
-                  </a>
-                </span>
-              </div>
+      {/* Partnership Wrapper */}
+      <div className="flex flex-wrap -m-4">
+        {partners.map((partner, index) => (
+          <PartnerWrapper key={index}>
+            <Image
+              alt={partner.name}
+              className="flex-shrink-0 w-48 object-contain object-center sm:mb-0 mb-4"
+              width={200}
+              height={200}
+              src={partner.imgSrc}
+            />
+            <div className="flex-grow pl-4">
+              <h2 className="title-font font-medium text-lg text-gray-900">
+                {partner.name}
+              </h2>
+              <h3 className="text-gray-500 mb-3">{partner.country}</h3>
+              <p className="mb-4">{partner.description}</p>
+              <SocialIcons links={partner.socialLinks} />
             </div>
-          </div>
-
-          {/* Partner 2 */}
-          <div className="p-4 lg:w-1/2">
-            <div className="h-full flex sm:flex-row flex-col items-start sm:justify-start justify-center text-center sm:text-left">
-              <Image
-                alt="team"
-                className="flex-shrink-0 w-48 object-contain object-center sm:mb-0 mb-4"
-                width={200}
-                height={200}
-                src="/images/partners/LAB.png"
-              />
-              <div className="flex-grow sm:pl-8">
-                <h2 className="title-font font-medium text-lg text-gray-900">
-                  LAB University of Applied Sciences
-                </h2>
-                <h3 className="text-gray-500 mb-3">Finland 🇫🇮</h3>
-                <p className="mb-4">
-                  LAB University of Applied Sciences in Finland stands out for its applied
-                  research and education in health and wellbeing, circular economy, and
-                  design, preparing students for impactful careers with a global
-                  perspective.
-                </p>
-                <span className="inline-flex">
-                  <a
-                    className="text-gray-500 hover:text-eu-blue"
-                    href="https://lab.fi/en/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaGlobe size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.facebook.com/LABfinland"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaFacebook size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.instagram.com/labfinland/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaInstagram size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.linkedin.com/school/labfinland/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaLinkedin size={24} />
-                  </a>
-                </span>
-              </div>
-            </div>
-          </div>
-
-          {/* Partner 3 */}
-          <div className="p-4 lg:w-1/2">
-            <div className="h-full flex sm:flex-row flex-col items-start sm:justify-start justify-center text-center sm:text-left">
-              <Image
-                alt="team"
-                className="flex-shrink-0 w-48 object-contain object-center sm:mb-0 mb-4"
-                width={200}
-                height={200}
-                src="/images/partners/unidu.png"
-              />
-              <div className="flex-grow sm:pl-8">
-                <h2 className="title-font font-medium text-lg text-gray-900">
-                  University of Dubrovnik
-                </h2>
-                <h3 className="text-gray-500 mb-3">Croatia 🇭🇷</h3>
-                <p className="mb-4">
-                  The University of Dubrovnik, Croatia, renowned for its maritime studies,
-                  electrical engineering, and business courses, combines rich historical
-                  heritage with modern educational approaches.
-                </p>
-                <span className="inline-flex">
-                  <a
-                    className="text-gray-500 hover:text-eu-blue"
-                    href="https://www.unidu.hr/eng/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaGlobe size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.facebook.com/UNIDUcro"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaFacebook size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.instagram.com/sveucilisteudubrovniku/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaInstagram size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.linkedin.com/school/unidu-cro/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaLinkedin size={24} />
-                  </a>
-                </span>
-              </div>
-            </div>
-          </div>
-
-          {/* Partner 4 */}
-          <div className="p-4 lg:w-1/2">
-            <div className="h-full flex sm:flex-row flex-col items-start sm:justify-start justify-center text-center sm:text-left">
-              {/* change the items-start to items-center to change vertical alignment of the image */}
-              <Image
-                alt="team"
-                className="flex-shrink-0 w-48 object-contain object-center sm:mb-0 mb-4"
-                width={200}
-                height={200}
-                src="/images/partners/formatio.png"
-              />
-              <div className="flex-grow sm:pl-8">
-                <h2 className="title-font font-medium text-lg text-gray-900">
-                  Formatio Private School
-                </h2>
-                <h3 className="text-gray-500 mb-3">Liechtenstein 🇱🇮</h3>
-                <p className="mb-4">
-                  Formatio Private School was founded in 1995 and pioneers personalized
-                  education, fosters strengths, and cultivates digital proficiency,
-                  preparing students for the 21st-century landscape with excellence and
-                  innovation.
-                </p>
-                <span className="inline-flex">
-                  <a
-                    className="text-gray-500 hover:text-eu-blue"
-                    href="https://www.formatio.li/en/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaGlobe size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.facebook.com/formatioprivatschule"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaFacebook size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.instagram.com/formatio_privatschule/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaInstagram size={24} />
-                  </a>
-                  <a
-                    className="text-gray-500 hover:text-eu-blue ml-4"
-                    href="https://www.linkedin.com/company/formatio-privatschule/"
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FaLinkedin size={24} />
-                  </a>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+          </PartnerWrapper>
+        ))}
       </div>
     </section>
   );
