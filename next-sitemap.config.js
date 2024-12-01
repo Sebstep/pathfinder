02f8d2863f6f78795c4543c9283e03c937f52f54
Sebastian Moder <moder.sebastian@gmail.com>
2024-12-01 22:44:22 +0100

    changed outputDir for sitemap to app

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,6 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://ai-pathfinder.eu",
   generateRobotsTxt: true, // (optional)
   generateIndexSitemap: false, // (optional)
+  outputDir: "app", // (optional)
   // ...other options
 };
